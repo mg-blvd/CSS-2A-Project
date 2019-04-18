1bef208da39e3c29650362a8db987e4d5568ab21
Misael Guijarro <misaelguijarro@gmail.com>
2019-04-18 17:50:46 +0100

    updated Panel

--- Panel.h
+++ Panel.h
@@ -1,12 +1,17 @@
 #ifndef PANEL_H
 #define PANEL_H
+#include <iostream>
+using namespace std;
 
 
 class Panel
 {
     public:
-        Panel();
-        char getDisplay()const {return display_char;}
+        Panel() {display_char = '=';}
+        Panel(char newChar) {display_char = newChar;}
+        char getChar()const {return display_char;}
+        void setChar(char newChar) {display_char = newChar;}
+         display()
 
     private:
         char display_char;
